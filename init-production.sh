6e5786d13a743fa7a7d327f223a5c2034ec813fd
vladino <vladino.vrab@gmail.com>
2018-08-05 10:32:50 +0200

    Fixed production auto deploy

--- init-production.sh
+++ init-production.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp -r /deploy/vendor /var/www/html && \
 chmod -R 777 /var/www/html/storage/logs && \
+php composer.phar install && \
 php artisan migrate --force && \
 crontab /var/www/html/crons/root && \
 cron && \
